d5dbede85bde85566d440205d084fdac1e8b8972
장정안 <wjddks96@naver.com>
2024-04-08 14:42:04 +0900

    feat: storage에 productData 추가

--- week2/js/storage.js
+++ week2/js/storage.js
@@ -1,3 +1,66 @@
-const storage = {};
+const storage = {
+  productData: [
+    {
+      id: 1,
+      category: "상의",
+      name: "검정반팔",
+      imageUrl: "./assets/검정 반팔.jpeg",
+    },
+    {
+      id: 2,
+      category: "상의",
+      name: "그레이반팔",
+      imageUrl: "./assets/그레이 반팔.jpeg",
+    },
+    {
+      id: 3,
+      category: "상의",
+      name: "체크셔츠",
+      imageUrl: "./assets/체크셔츠.jpeg",
+    },
+    {
+      id: 4,
+      category: "상의",
+      name: "흰색반팔",
+      imageUrl: "./assets/흰색 반팔.jpeg",
+    },
+    {
+      id: 5,
+      category: "신발",
+      name: "검정 어글리슈즈",
+      imageUrl: "./assets/검정 어글리슈즈.png",
+    },
+    {
+      id: 6,
+      category: "신발",
+      name: "뉴발996",
+      imageUrl: "./assets/뉴발996.jpeg",
+    },
+    {
+      id: 7,
+      category: "신발",
+      name: "뉴발2002",
+      imageUrl: "./assets/뉴발2002.jpeg",
+    },
+    {
+      id: 8,
+      category: "신발",
+      name: "아디다스다크블루",
+      imageUrl: "./assets/아디다스다크블루.jpeg",
+    },
+    {
+      id: 9,
+      category: "도서",
+      name: "리액트deepdive",
+      imageUrl: "./assets/리액트 deepdive.jpeg",
+    },
+    {
+      id: 10,
+      category: "도서",
+      name: "자스deepdive",
+      imageUrl: "./assets/자스 deepdive.jpeg",
+    },
+  ],
+};
 
 export default storage;
