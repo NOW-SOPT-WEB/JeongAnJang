f799fd14c39f98b7737512d2d99dde5122fa6668
장정안 <wjddks96@naver.com>
2024-04-11 00:34:47 +0900

    feat: NavView에 props 전달

--- week2/src/js/views/NavView.js
+++ week2/src/js/views/NavView.js
@@ -1,57 +1,19 @@
 import View from "./View.js";
 import storage from "../storage.js";
-import { qs } from "../utils/domHelper.js";
 
 export default class NavView extends View {
   setEvent() {
+    const { filterCategory, renderFilterdProducts } = this.props;
+
     this.addEvent("click", ".all", () =>
-      this.renderFilterdProducts(storage.productData)
+      renderFilterdProducts(storage.productData)
     );
-    this.addEvent("click", ".cloth", () => this.filterCategory("shirt"));
-    this.addEvent("click", ".shoes", () => this.filterCategory("shoes"));
-    this.addEvent("click", ".books", () => this.filterCategory("books"));
-  }
-
-  filterCategory(category) {
-    const filterdProducts = storage.productData.filter(
-      (items) => items.category === category
-    );
-    this.renderFilterdProducts(filterdProducts);
-  }
-
-  renderFilterdProducts(products) {
-    const mainSection = qs("main");
-
-    mainSection.innerHTML = `<section id=${products[0].category}>
-    <h2>${products[0].category}</h2>
-    <div class="product_card_wrapper">
-        ${this.getFilteredProductsTemplate(products)}
-    </div>
-  </section>`;
-  }
-
-  getFilteredProductsTemplate(products) {
-    return products
-      .map(
-        (items) =>
-          `
-      <div class="product_card">
-        <img
-        class="product_card_img"
-        src="${items.imageUrl}"
-        alt="${items.name}"
-        />
-        <i class="fa-solid fa-heart"></i>
-        <p>${items.name}</p>
-        <p>${items.price}</p>
-    </div>
-          `
-      )
-      .join("");
+    this.addEvent("click", ".cloth", () => filterCategory("shirt"));
+    this.addEvent("click", ".shoes", () => filterCategory("shoes"));
+    this.addEvent("click", ".books", () => filterCategory("books"));
   }
 
   template() {
-    console.log("NavView 내 template 실행");
     return `
         <h2>판매 목록</h2>
         <ul>
