3c7e13d4e5bd9f8b025e7ab0049ef2da12417445
장정안 <wjddks96@naver.com>
2024-04-10 14:01:48 +0900

    feat:  View 클래스 내 이벤트 관리 함수 작성

--- week2/src/js/views/View.js
+++ week2/src/js/views/View.js
@@ -1,19 +1,23 @@
-const tag = "Views";
+import { on, qsAll } from "../utils/domHelper";
 
 export default class View {
   constructor(element) {
-    console.log(tag);
+    console.log("View 실행");
+    this.element = element;
 
     if (!element) throw "no element";
-    this.element = element;
+    this.setEvent();
     this.render();
   }
+
+  setEvent() {}
 
   show(className) {
     this.element.classList.add(className);
   }
 
   render() {
+    console.log("View 내 render실행");
     this.element.innerHTML = this.template();
     this.mounted();
   }
@@ -23,4 +27,16 @@ export default class View {
   }
 
   mounted() {}
+
+  addEvent(eventType, selector, callback, element = this.element) {
+    const children = [...qsAll(selector, element)];
+    const isTarget = (element) =>
+      children.includes(element) || element.closest(selector);
+
+    element.addEventListener(eventType, (event) => {
+      if (isTarget(event.target)) callback(event);
+    });
+
+    return this;
+  }
 }
