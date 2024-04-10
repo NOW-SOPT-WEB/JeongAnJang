01d9470996f6cd31b0db90b68cc1cd2acf9348f9
장정안 <wjddks96@naver.com>
2024-04-11 00:34:26 +0900

    feat: App 클래스 props 구조로 수정

--- week2/src/js/app.js
+++ week2/src/js/app.js
@@ -1,52 +1,76 @@
-import storage from "./storage";
 import { qs } from "./utils/domHelper";
+import HeaderView from "./views/HeaderView";
 import NavView from "./views/NavView";
 import View from "./views/View";
 import "../css/style.css";
+import BannerView from "./views/BannerView";
 import MainSectionView from "./views/MainSectionView";
-import HeaderView from "./views/HeaderView";
+import storage from "./storage";
 
 export default class App extends View {
   constructor() {
     super(qs("#app"));
-    console.log("App 실행");
   }
 
   mounted() {
-    console.log("App 내 mounted 실행");
-    new HeaderView(qs(".header"));
-    new NavView(qs(".nav"));
-    new MainSectionView(qs("main"));
+    const { filterCategory, renderFilterdProducts } = this;
+
+    new HeaderView(qs("header"));
+    new BannerView(qs(".slide_animation_section"));
+    new NavView(qs(".nav"), {
+      filterCategory: filterCategory.bind(this),
+      renderFilterdProducts: renderFilterdProducts.bind(this),
+    });
+    new MainSectionView(qs(".main"));
   }
 
   template() {
-    console.log("App 내 template실행!!");
-
     return `
+    <header class="header"></header>
     <section class="slide_section_wrapper">
-      <div class="slide_animation_section">
-        <ul class="banner_list n1">
-         ${this.getBannerSlideTemplate()}
-        </ul>
-        <ul class="banner_list n2">
-         ${this.getBannerSlideTemplate()}
-        </ul>
-      </div>
-    </section> 
-        `;
+      <div class="slide_animation_section"></div>
+    </section>
+    <nav class="nav"></nav>
+    <main class="main"></main>
+    <footer></footer>
+    `;
   }
 
-  getBannerSlideTemplate() {
-    return storage.productData
+  filterCategory(category) {
+    const filterdProducts = storage.productData.filter(
+      (items) => items.category === category
+    );
+
+    this.renderFilterdProducts(filterdProducts);
+  }
+
+  renderFilterdProducts(products) {
+    const mainSection = qs("main");
+
+    mainSection.innerHTML = `<section id=${products[0].category}>
+    <h2>${products[0].category}</h2>
+    <div class="product_card_wrapper">
+        ${this.getFilteredProductsTemplate(products)}
+    </div>
+  </section>`;
+  }
+
+  getFilteredProductsTemplate(products) {
+    return products
       .map(
         (items) =>
-          `<li>
-            <img 
-            class="slide_img"
-            src="${items.imageUrl}"
-            alt="${items.name}"
-            />
-        </li>`
+          `
+      <div class="product_card">
+        <img
+        class="product_card_img"
+        src="${items.imageUrl}"
+        alt="${items.name}"
+        />
+        <i class="fa-solid fa-heart"></i>
+        <p>${items.name}</p>
+        <p>${items.price}</p>
+    </div>
+          `
       )
       .join("");
   }
