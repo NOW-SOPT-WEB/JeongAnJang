cd63c91adf8f454981920e1483d299a1814bdebf
장정안 <wjddks96@naver.com>
2024-04-17 02:54:16 +0900

    feat: 구매 모달 뷰 템플릿 초기 구현

---
+++ week2/src/js/views/ModalView.js
@@ -0,0 +1,15 @@
+import View from "./View";
+
+export default class ModalView extends View {
+  template() {
+    return `
+            <dialog class="product_list_modal">
+                <div>asdfasdf</div>
+            </dialog>
+        `;
+  }
+
+  getSelectedProductTemplate() {
+    
+  }
+}
