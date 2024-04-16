62906858b7ca99078a40206781e4f2056df0fd0a
장정안 <wjddks96@naver.com>
2024-04-17 05:04:47 +0900

    fix: 장바구니 담는 메세지 수정

--- week2/src/js/constants.js
+++ week2/src/js/constants.js
@@ -1,7 +1,7 @@
 export const MESSAGES = Object.freeze({
   CONFIRM_ADD_CART: "상품을 장바구니에 담으시겠습니까?",
-  COMPLETE_ADD_CART: "상품이 장바구니에 담겼습니다.",
-  CANCEL_ADD_CART: "취소 되었습니다."
+  COMPLETE_ADD_CART: "상품이 장바구니에 담겼습니다. 장바구니로 이동합니다.",
+  CANCEL_ADD_CART: "취소 되었습니다.",
 });
 
 export const HOME = "index.html";
