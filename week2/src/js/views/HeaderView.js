86a8529e2af98d688257bf41696bfba27bdc3762
장정안 <wjddks96@naver.com>
2024-04-18 17:27:33 +0900

    fix: 과제 요구사항에 맞게 사이드 바 내 텍스트 변경

--- week2/src/js/views/HeaderView.js
+++ week2/src/js/views/HeaderView.js
@@ -4,52 +4,29 @@ import CartView from "./CartView";
 import View from "./View";
 
 export default class HeaderView extends View {
-  constructor(element, props) {
-    super(element, props);
-    this.sideBarOpen = false;
-    console.log("헤더뷰 내 props", this.props);
+  setUp() {
+    console.log("헤더 뷰 내 this", this);
+    this.navigateHome = this.props.navigateHome;
+    this.navigateCart = this.props.navigateCart;
+    this.cartList = this.props.cartList;
   }
 
   setEvent() {
-    const { navigateHome } = this.props;
-    this.addEvent("click", ".fa-house", navigateHome);
-    this.addEvent("click", ".fa-bars", this.toggleSideBar.bind(this));
-    this.addEvent(
-      "click",
-      ".side_bar_close_btn",
-      this.toggleSideBar.bind(this)
-    );
+    console.log("헤더 뷰 내 this.props", this.props);
+    console.log("헤더 뷰 내 this.cartList", this.cartList);
+
+    this.addEvent("click", ".fa-house", this.navigateHome);
+    this.addEvent("click", ".fa-bars", this.openSideBar.bind(this));
+    this.addEvent("click", ".side_bar_close_btn", this.closeSideBar.bind(this));
     this.addEvent("click", ".side_bar_product", this.navigateCart);
-  }
-
-  navigateCart() {
-    // const { cartList } = this.props;
-    // console.log("헤더뷰 내 카트리스트", cartList);
-    const appPage = document.getElementById("app");
-    appPage.innerHTML = "";
-
-    new CartView(qs("#app"));
-  }
-
-  toggleSideBar() {
-    if (this.sideBarOpen) {
-      console.log(this.sideBarOpen);
-      this.closeSideBar();
-    } else {
-      console.log(this.sideBarOpen);
-      this.openSideBar();
-    }
   }
 
   openSideBar() {
     qs(".side_bar").classList.add("open");
-    this.sideBarOpen = true;
   }
 
   closeSideBar() {
     qs(".side_bar").classList.remove("open");
-
-    this.sideBarOpen = false;
   }
 
   template() {
@@ -63,8 +40,9 @@ export default class HeaderView extends View {
                 <button class="side_bar_close_btn">SideBar Close</button>
               </div>
                <ul>
-                <li class="side_bar_home">Home</li>
-                <li class="side_bar_product">Product</li>
+                <li class="side_bar_interest">관심상품보기</li>
+                <li class="side_bar_product">장바구니</li>
+                <li class="side_bar_admin">관리자</li>
               </ul>
               </aside>
              </div>
