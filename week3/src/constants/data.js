7f4bc643260d34b92ae53763c17008823373f157
장정안 <wjddks96@naver.com>
2024-04-26 14:55:48 +0900

    feat: 데이터 추가

---
+++ week3/src/constants/data.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    id: 1,
+    item: "스윗",
+    status: false,
+  },
+  {
+    id: 2,
+    item: "마일",
+    status: false,
+  },
+  {
+    id: 3,
+    item: "레큐",
+    status: false,
+  },
+  {
+    id: 4,
+    item: "토스터",
+    status: false,
+  },
+  {
+    id: 5,
+    item: "문샷",
+    status: false,
+  },
+  {
+    id: 6,
+    item: "모디",
+    status: false,
+  },
+  {
+    id: 7,
+    item: "논술메이트",
+    status: false,
+  },
+  {
+    id: 8,
+    item: "돈비",
+    status: false,
+  },
+  {
+    id: 9,
+    item: "두립",
+    status: false,
+  },
+  {
+    id: 10,
+    item: "핑글",
+    status: false,
+  },
+  {
+    id: 11,
+    item: "소프티",
+    status: false,
+  },
+  {
+    id: 12,
+    item: "하면함",
+    status: false,
+  },
+  {
+    id: 13,
+    item: "모티부",
+    status: false,
+  },
+  {
+    id: 14,
+    item: "웹소소",
+    status: false,
+  },
+  {
+    id: 15,
+    item: "ASAP",
+    status: false,
+  },
+  {
+    id: 16,
+    item: "타투어",
+    status: false,
+  },
+  {
+    id: 17,
+    item: "팜스프링",
+    status: false,
+  },
+  {
+    id: 18,
+    item: "옐로",
+    status: false,
+  },
+];
