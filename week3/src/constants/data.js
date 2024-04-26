e0ffc783086435ceb5114354ba6b68a1d310c437
장정안 <wjddks96@naver.com>
2024-04-26 16:21:02 +0900

    feat: 카드 이미지 import

--- week3/src/constants/data.js
+++ week3/src/constants/data.js
@@ -1,92 +1,68 @@
-export const data = [
+import {
+  doorip,
+  hmh,
+  lecue,
+  mile,
+  moddy,
+  moonshot,
+  nonsool,
+  sweet,
+  toaster,
+} from "../assets";
+
+export const CARD_DATA = [
   {
     id: 1,
     item: "스윗",
+    src: sweet,
     status: false,
   },
   {
     id: 2,
     item: "마일",
+    src: mile,
     status: false,
   },
   {
     id: 3,
     item: "레큐",
+    src: lecue,
     status: false,
   },
   {
     id: 4,
     item: "토스터",
+    src: toaster,
     status: false,
   },
   {
     id: 5,
     item: "문샷",
+    src: moonshot,
     status: false,
   },
   {
     id: 6,
     item: "모디",
+    src: moddy,
     status: false,
   },
   {
     id: 7,
     item: "논술메이트",
+    src: nonsool,
     status: false,
   },
   {
     id: 8,
-    item: "돈비",
+    item: "하면함",
+    src: hmh,
     status: false,
   },
   {
     id: 9,
     item: "두립",
-    status: false,
-  },
-  {
-    id: 10,
-    item: "핑글",
-    status: false,
-  },
-  {
-    id: 11,
-    item: "소프티",
-    status: false,
-  },
-  {
-    id: 12,
-    item: "하면함",
-    status: false,
-  },
-  {
-    id: 13,
-    item: "모티부",
-    status: false,
-  },
-  {
-    id: 14,
-    item: "웹소소",
-    status: false,
-  },
-  {
-    id: 15,
-    item: "ASAP",
-    status: false,
-  },
-  {
-    id: 16,
-    item: "타투어",
-    status: false,
-  },
-  {
-    id: 17,
-    item: "팜스프링",
-    status: false,
-  },
-  {
-    id: 18,
-    item: "옐로",
+    src: doorip,
     status: false,
   },
 ];
