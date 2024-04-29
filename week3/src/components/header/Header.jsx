b12c68ec36d76a04ccc9dae1b4629cf25d33882f
장정안 <wjddks96@naver.com>
2024-04-29 18:09:33 +0900

    feat: 난이도 별로 completed 숫자 변경

--- week3/src/components/header/Header.jsx
+++ week3/src/components/header/Header.jsx
@@ -1,15 +1,14 @@
 import Button from "../@common/button/button";
 import * as S from "./Header.style";
-/**@todo 컴포넌트 합성으로 해볼까 말까
- *
- * 버튼에 onClick 달기, 게임 상태에 따라 props 내려줘야할듯
- */
-export default function Header() {
+export default function Header(props) {
+  const { resetGame, turns, completed } = props;
   return (
     <S.HeaderWrapper>
       <S.HeaderTitle>OB들 서비스 맞추기</S.HeaderTitle>
-      <Button>RESET</Button>
-      <S.GameState>0 / 5</S.GameState>
+      <Button onClick={resetGame}>RESET</Button>
+      <S.GameState>
+        {turns} / {completed}
+      </S.GameState>
     </S.HeaderWrapper>
   );
 }
