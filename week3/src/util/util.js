5dad8fe0cbd7332d61394c9ea79bbd4b20ea5863
장정안 <wjddks96@naver.com>
2024-04-29 18:08:38 +0900

    feat: 카드 섞기 유틸 함수 작성

---
+++ week3/src/util/util.js
@@ -0,0 +1,8 @@
+import { NUMBER } from "../constants/constants";
+
+export const shuffleCards = (array) => {
+  return array
+    .concat(array)
+    .sort(() => Math.random() - NUMBER.SHUFFLE_HELP_NUM)
+    .map((card) => ({ ...card, id: Math.random() }));
+};
