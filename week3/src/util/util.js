a75da9b12a9598d74cedb2f23bbda8f4125f0d8d
장정안 <wjddks96@naver.com>
2024-04-30 20:09:47 +0900

    feat: 비지니스 로직 -> 유틸함수로 분리

--- week3/src/util/util.js
+++ week3/src/util/util.js
@@ -1,7 +1,58 @@
 import { NUMBER } from "../constants/constants";
+import { CARD_DATA } from "../constants/data";
 
 export const shuffleCards = (array) => {
   return array
     .sort(() => Math.random() - NUMBER.SHUFFLE_HELP_NUM)
     .map((card) => ({ ...card, id: Math.random() }));
+};
+
+export const generateCardsByLevel = (level, setCompleted) => {
+  let totalPairs;
+  switch (level) {
+    case "Easy":
+      totalPairs = NUMBER.EASY_LEVEL;
+      setCompleted(NUMBER.EASY_LEVEL);
+      break;
+    case "Normal":
+      totalPairs = NUMBER.NORMAL_LEVEL;
+      setCompleted(NUMBER.NORMAL_LEVEL);
+      break;
+    case "Hard":
+      totalPairs = NUMBER.HARD_LEVEL;
+      setCompleted(NUMBER.HARD_LEVEL);
+      break;
+    default:
+      totalPairs = NUMBER.EASY_LEVEL;
+      break;
+  }
+
+  const cardPairs = CARD_DATA.slice(0, totalPairs);
+  const cards = [...cardPairs, ...cardPairs];
+
+  return shuffleCards(cards);
+};
+
+export const compareCards = (
+  choices,
+  cards,
+  setCards,
+  setTurns,
+  resetCardValue
+) => {
+  const isMatch = choices[0]?.name === choices[1]?.name;
+  console.log("compareCards 내 cards", cards);
+  if (choices[0] && choices[1]) {
+    if (isMatch) {
+      setCards((prevCards) =>
+        prevCards.map((card) =>
+          card.name === choices[0].name ? { ...card, status: true } : card
+        )
+      );
+      setTurns((prevTurns) => prevTurns + 1);
+      resetCardValue();
+    } else {
+      setTimeout(resetCardValue, 1000);
+    }
+  }
 };
