b615f73f7db550b6ff717cccc0de3eba99b2ced4
장정안 <wjddks96@naver.com>
2024-04-30 20:09:29 +0900

    style: Card 컴포넌트 스타일 코드와 분리

--- week3/src/components/card/Card.jsx
+++ week3/src/components/card/Card.jsx
@@ -1,5 +1,6 @@
-import styled from "styled-components";
 import cover from "../../assets/cover.png";
+import * as S from "./Card.style.js";
+
 const Card = (props) => {
   const { card, handleChoice, flipped } = props;
 
@@ -10,56 +11,18 @@ const Card = (props) => {
   };
 
   return (
-    <CardContainer className="card">
+    <S.CardContainer className="card">
       <div>
-        <FrontImage src={card.src} alt="card front" $flipped={flipped} />
-        <BackImage
+        <S.FrontImage src={card.src} alt="card front" $flipped={flipped} />
+        <S.BackImage
           src={cover}
           onClick={handleClick}
           alt="card back"
           $flipped={flipped}
         />
       </div>
-    </CardContainer>
+    </S.CardContainer>
   );
 };
 
 export default Card;
-
-const CardContainer = styled.article`
-  position: relative;
-`;
-
-const CardImage = styled.img`
-  width: 60%;
-  display: block;
-  border: 2px solid #fff;
-  border-radius: 6px;
-`;
-
-const FrontImage = styled(CardImage)`
-  transform: rotateY(90deg);
-  transition: all ease-in 0.2s;
-  position: absolute;
-  cursor: pointer;
-
-  ${({ $flipped }) =>
-    $flipped &&
-    `
-    transform: rotateX(0deg);
-    transition-delay: 0.2s;
-  `}
-`;
-
-const BackImage = styled(CardImage)`
-  transition: all ease-in 0.2s;
-  transition-delay: 0.2s;
-  cursor: pointer;
-
-  ${({ $flipped }) =>
-    $flipped &&
-    `
-    transform: rotateY(90deg); 
-    transition-delay: 0s;
-  `}
-`;
