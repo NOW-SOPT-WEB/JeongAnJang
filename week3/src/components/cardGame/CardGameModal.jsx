014bc8f8f515a27c3e0bbd8af33bd0d5efa35ca9
장정안 <wjddks96@naver.com>
2024-05-05 17:54:10 +0900

    refactor: 필요없는 함수 제거

--- week3/src/components/cardGame/CardGameModal.jsx
+++ week3/src/components/cardGame/CardGameModal.jsx
@@ -3,15 +3,11 @@ import Modal from "../@common/modal/Modal";
 const CardGameModal = (props) => {
   const { resetGame } = props;
 
-  const handleClose = () => {
-    resetGame();
-  };
-
   return (
     <Modal>
       <Modal.BackDrop />
       <Modal.Content>
-        <Modal.Close onClick={handleClose}>x</Modal.Close>
+        <Modal.Close onClick={resetGame}>x</Modal.Close>
         <h1>게임 끝!!! </h1>
       </Modal.Content>
     </Modal>
