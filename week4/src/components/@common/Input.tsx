8a480e44c1b20612f2ec108ec8790d90a94b75dc
장정안 <wjddks96@naver.com>
2024-05-08 17:16:16 +0900

    feat: 공통 input 구현

---
+++ week4/src/components/@common/Input.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+const Input = (props: InputProps) => {
+  const { onChange } = props;
+  return (
+    <>
+      <CommonInput type="text" onChange={onChange} />
+    </>
+  );
+};
+
+export default Input;
+
+export const CommonInput = styled.input`
+  ${({ theme }) =>
+    theme.mixin.flexBox({ align: "center", justify: "space-between" })};
+  width: 100%;
+  height: 3.5rem;
+  outline: none;
+  padding: 0;
+  color: black;
+
+  input::placeholder {
+    color: gray;
+  }
+`;
