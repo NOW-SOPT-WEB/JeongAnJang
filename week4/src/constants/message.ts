0398183d2b4d19938436bb15056d678148044bd6
장정안 <wjddks96@naver.com>
2024-05-08 23:31:43 +0900

    feat: 메세지 추가

--- week4/src/constants/message.ts
+++ week4/src/constants/message.ts
@@ -2,4 +2,5 @@ export const MESSAGE = Object.freeze({
   ENTER_EXIST_PASSWORD: "기존 비밀번호를 입력해주세요.",
   ENTER_NEW_PASSWORD: "새로운 비밀번호를 입력해주세요.",
   ENTER_NEW_PASSWORD_VERIFICATION: "비밀번호 확인을 입력해주세요.",
+  SUCCESS_NEW_PASSWORD: "비밀번호 변경 성공",
 });
