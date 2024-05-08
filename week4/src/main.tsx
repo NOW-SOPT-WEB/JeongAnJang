234d00e16612d5a31386ceb87600d0d21dc3bc6f
장정안 <wjddks96@naver.com>
2024-05-08 15:55:37 +0900

    feat: 글로벌스타일 theme mixin 설정

--- week4/src/main.tsx
+++ week4/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import Styles from "./styles/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Styles>
+      <App />
+    </Styles>
   </React.StrictMode>
 );
