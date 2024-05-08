87d36abbc1eddd175ccd6072a6a019e19e2faea8
장정안 <wjddks96@naver.com>
2024-05-08 15:56:04 +0900

    feat: 공통 버튼 추가

---
+++ week4/src/components/@common/Button.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button = (props: ButtonProps) => {
+  const { children, ...rest } = props;
+  return (
+    <Wrapper>
+      <button {...rest}>{children} </button>
+    </Wrapper>
+  );
+};
+
+export default Button;
+
+const Wrapper = styled.div`
+  ${({ theme: { mixin } }) =>
+    mixin.inlineFlexBox({ align: "center", justify: "center" })}
+
+  height: 4.4rem;
+  padding: 1rem 1.1rem 1rem 2rem;
+  border-radius: 9.9rem;
+
+  background-color: pink;
+  color: white;
+  border: none;
+`;
--- week4/src/pages/MainPage.tsx
+++ week4/src/pages/MainPage.tsx
@@ -1,7 +1,26 @@
+import ReactPlayer from "react-player";
+import Button from "../components/@common/Button";
+import useEasyNavigate from "../hooks/@common/useEasyNavigate";
+import styled from "styled-components";
+
 interface MainPageProps {}
 
 const MainPage = ({}: MainPageProps) => {
-  return <></>;
+  const { goMypage, goSignup } = useEasyNavigate();
+  return (
+    <div>
+      <ReactPlayer url="https://www.youtube.com/shorts/mZPkoLfdGQg" />
+      <ButtonWrapper>
+        <Button onClick={goSignup}>회원가입</Button>
+        <Button onClick={goMypage}>내 정보</Button>
+      </ButtonWrapper>
+    </div>
+  );
 };
 
 export default MainPage;
+
+const ButtonWrapper = styled.div`
+  ${({ theme }) => theme.mixin.flexCenter({ direction: "row" })};
+  gap: 5rem;
+`;
