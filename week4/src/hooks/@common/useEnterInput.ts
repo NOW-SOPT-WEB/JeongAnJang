2d054ea5bc77cff04e0bdbb59bf21211fb9102aa
장정안 <wjddks96@naver.com>
2024-05-10 02:11:00 +0900

    feat: 전화번호 입력시 자동 '-' 생성

--- week4/src/hooks/@common/useEnterInput.ts
+++ week4/src/hooks/@common/useEnterInput.ts
@@ -19,7 +19,17 @@ const useEnterInput = () => {
   };
 
   const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPhone(e.target.value);
+    const phoneNumber = e.target.value;
+    // 숫자와 '-'를 제외한 문자 제거
+    const cleanedPhoneNumber = phoneNumber.replace(/[^\d-]/g, "");
+    // '-'를 제외한 모든 숫자 추출
+    const numbersOnly = cleanedPhoneNumber.replace(/-/g, "");
+    // 전화번호 형식에 맞게 '-' 추가
+    const formattedPhoneNumber = numbersOnly.replace(
+      /(\d{3})(\d{3,4})(\d{4})/,
+      "$1-$2-$3"
+    );
+    setPhone(formattedPhoneNumber);
   };
   return {
     id,
