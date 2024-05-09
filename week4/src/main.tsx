18414779db1c241e6dc3628d4b7944e49f09c32a
장정안 <wjddks96@naver.com>
2024-05-10 07:37:17 +0900

    feat: Context API로 memberId 관리

--- week4/src/main.tsx
+++ week4/src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import Styles from "./styles/index.tsx";
+import { MemberContextProvider } from "./context/MemberContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Styles>
-      <App />
+      <MemberContextProvider>
+        <App />
+      </MemberContextProvider>
     </Styles>
   </React.StrictMode>
 );
