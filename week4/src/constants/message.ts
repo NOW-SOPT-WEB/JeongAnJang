1d4d5b5f5198841ded80394172cacd5a1d874dbd
장정안 <wjddks96@naver.com>
2024-05-09 15:12:26 +0900

    feat: 에러 메세지 추가

--- week4/src/constants/message.ts
+++ week4/src/constants/message.ts
@@ -3,4 +3,10 @@ export const MESSAGE = Object.freeze({
   ENTER_NEW_PASSWORD: "새로운 비밀번호를 입력해주세요.",
   ENTER_NEW_PASSWORD_VERIFICATION: "비밀번호 확인을 입력해주세요.",
   SUCCESS_NEW_PASSWORD: "비밀번호 변경 성공",
+  SUCCESS_LOGIN: "로그인 성공",
+  FAIL_LOGIN: "로그인에 실패했습니다.",
+  SUCCESS_SIGNUP: "회원가입 성공",
+  ALREADY_EXIST_ID: "중복된 아이디 입니다.",
+  NOW_FOLLOW_PASSWORD_FORMAT: "비밀번호 형식을 지켜주세요.",
+  NOW_FOLLOW_PHONE_FORMAT: "전화번호 형식을 지켜주세요.",
 });
