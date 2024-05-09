9ec04603f968ccc52b58028e8e613aee31f75ced
장정안 <wjddks96@naver.com>
2024-05-10 05:19:10 +0900

    feat: 아래 두 영상의 차이점은?

--- week4/src/pages/MainPage.tsx
+++ week4/src/pages/MainPage.tsx
@@ -1,11 +1,12 @@
 import ReactPlayer from "react-player";
+import leeJinChu from "../assets/이진츄.mp4";
 
-interface MainPageProps {}
-
-const MainPage = ({}: MainPageProps) => {
+const MainPage = () => {
   return (
     <>
+      <h1 style={{ fontSize: "9rem" }}>아래 두 영상의 차이점은?</h1>
       <ReactPlayer url="https://www.youtube.com/shorts/mZPkoLfdGQg" />
+      <ReactPlayer url={leeJinChu} playing controls />
     </>
   );
 };
