701359f8041b211f11d2b58a4fe6e534ad03ee55
장정안 <wjddks96@naver.com>
2024-05-10 02:11:32 +0900

    feat: 입력 요청 메세지 추가

--- week4/src/constants/message.ts
+++ week4/src/constants/message.ts
@@ -1,4 +1,8 @@
 export const MESSAGE = Object.freeze({
+  ENTER_EMPTY_ID: "아이디 입력해주세요",
+  ENTER_EMPTY_PASSWORD: "비밀번호 입력해주세요",
+  ENTER_EMPTY_NICKNAME: "닉네임 입력해주세요",
+  ENTER_EMPTY_PHONE: "전화번호 입력해주세요.",
   ENTER_EXIST_PASSWORD: "기존 비밀번호를 입력해주세요.",
   ENTER_NEW_PASSWORD: "새로운 비밀번호를 입력해주세요.",
   ENTER_NEW_PASSWORD_VERIFICATION: "비밀번호 확인을 입력해주세요.",
